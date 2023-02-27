03361f1b3903834484b6f54762dceab78c9dbdb2
peter988 <116488083+peter988@users.noreply.github.com>
2023-02-27 14:59:28 +0200

    Update settings.py

--- facebook/settings.py
+++ facebook/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-89+=67a4x%#5mm&2__tjgr5(=f*e26v^(@lku@q)l_p2h8bh46"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
